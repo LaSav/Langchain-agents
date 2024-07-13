5b931851d5d1d8cf9f9ce250acf6436b8193f95c
savva <savva.lambin@gmail.com>
2024-07-13 15:36:59 +0100

    Add: chat model start handler

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
 
-chat = ChatOpenAI()
+handler = ChatModelStartHandler()
+chat = ChatOpenAI(
+  callbacks=[handler]
+)
 
 tables = list_tables()
 prompt = ChatPromptTemplate(
@@ -24,11 +29,13 @@ prompt = ChatPromptTemplate(
       "Do not make any assumptions about what tables exist"
       "or what columns exists. Instead, use the 'describe_tables' function"
       )),
+    MessagesPlaceholder(variable_name="chat_history"),
     HumanMessagePromptTemplate.from_template("{input}"),
     MessagesPlaceholder(variable_name='agent_scratchpad')
   ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(
@@ -39,8 +46,11 @@ agent = OpenAIFunctionsAgent(
 
 agent_executor = AgentExecutor(
   agent=agent,
-  verbose=True,
-  tools=tools
+  # verbose=True,
+  tools=tools,
+  memory=memory
 )
 
-agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+agent_executor("How many orders are there? Write the results to a report file.")
+
+agent_executor("Repeat the exact same process for users.")
